f86093dd1fd646c4c1a951a2956587deb27b6dd2
rokataku <t>
2021-01-05 22:02:28 +0900

    既読機能のサーバーサイドの実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'posts#index'
+  get 'posts/:post_id/comments/:id', to: 'posts#checked'
   resources :posts do
     resources :comments, only: :create
   end
