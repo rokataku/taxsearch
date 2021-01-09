b95e1833994c2c864ce6eacb90632044bfc6d196
rokataku <t>
2021-01-09 16:29:54 +0900

    Active Storageの導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,8 @@
 class Post < ApplicationRecord
   validates :text, presence: true
   belongs_to :user
-  has_many :comments 
+  has_many :comments
+  has_one_attached :image
+
+  validates :title, presense: true
 end
